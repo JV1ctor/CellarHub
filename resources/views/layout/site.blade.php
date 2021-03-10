bb80a2b446e41a64edcf43c84970cecfca83704d
guiga07 <guicm007@gmail.com>
2021-03-09 23:11:58 -0300

    adicionando layouts, dependências e alguns ajustes

---
+++ resources/views/layout/site.blade.php
@@ -0,0 +1,6 @@
+
+@include('layout._includes.header') <!-- chama o layout do topo -->
+
+@yield('conteudo') <!-- abre section com mesmo nome para pôr o conteudo aqui -->
+
+@include('layout._includes.footer') <!-- chama o layout do rodape -->
